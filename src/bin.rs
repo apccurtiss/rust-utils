95aab5cd26ae4883d122962997f1c0bb06c01851
Lalaithion <izaak.weiss@gmail.com>
2018-08-19 21:26:26 -0600

    Added example program

--- src/bin.rs
+++ src/bin.rs
@@ -1,5 +1,15 @@
 extern crate ad_lib;
 
+use ad_lib::*;
+
 fn main() {
-    println!("{}", ad_lib::greetings());
+    let test = from_vec(vec![1,2,3,4,5,6,7]);
+    let test2 = test.set(4, &202);
+    let test3 = test.append(&16);
+    let test4 = test.concat(&test);
+
+    println!("{:?}", test.flatten());
+    println!("{:?}", test2.flatten());
+    println!("{:?}", test3.flatten());
+    println!("{:?}", test4.flatten());
 }

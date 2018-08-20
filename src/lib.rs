ac398b341e4c241a9fc8db53c0a15b1c0164f38f
Lalaithion <izaak.weiss@gmail.com>
2018-08-19 21:26:07 -0600

    Added new instances and boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,79 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-struct VectorWrapper<A> {
+pub struct VectorWrapper<A> {
     vector: Vec<A>
 }
 
-struct Concatenated<'a, 'b, A: 'b + 'a> {
-    left: &'a ListThingy<A>,
-    right: &'b ListThingy<A>
+pub struct Concatenated<'a, 'b, A: 'a + 'b> {
+    left: &'a Persistent<A>,
+    right: &'b Persistent<A>
 }
 
-trait ListThingy<A> {
-    fn concat<'a, 'b>(&'a self, v: &'b ListThingy<A>) -> Concatenated<'a, 'b, A>;
+pub struct Prepended<'a, 'b, A: 'a + 'b> {
+    value: &'b A,
+    rest: &'a Persistent<A>
+}
+
+pub struct Appended<'a, 'b, A: 'a + 'b> {
+    rest: &'a Persistent<A>,
+    value: &'b A
+}
+
+pub struct Changed<'a, 'b, A: 'a + 'b> {
+    rest: &'a Persistent<A>,
+    index: usize,
+    value: &'b A
+}
+
+pub fn from_vec<A>(v: Vec<A>) -> VectorWrapper<A> {
+    VectorWrapper{vector : v}
+}
+
+pub trait Persistent<A> {
+    fn concat<'a, 'b>(&'a self, v: &'b Persistent<A>) -> Concatenated<'a, 'b, A>;
+    fn prepend<'a, 'b>(&'a self, v: &'b A) -> Prepended<'a, 'b, A>;
+    fn append<'a, 'b>(&'a self, v: &'b A) -> Appended<'a, 'b, A>;
+    fn set<'a, 'b>(&'a self, i: usize, v: &'b A) -> Changed<'a, 'b, A>;
     fn index(&self, i: usize) -> &A;
     fn length(&self) -> usize;
+
+    fn flatten(&self) -> Vec<&A> {
+        let mut v = Vec::with_capacity(self.length());
+        for i in 0..self.length() {
+            v.push(self.index(i));
+        }
+        return v;
+    }
 }
 
-impl<A> ListThingy<A> for VectorWrapper<A> {
-    fn concat<'a, 'b>(&'a self, v: &'b ListThingy<A>) -> Concatenated<'a, 'b, A> {
+impl<A> Persistent<A> for VectorWrapper<A> {
+    fn concat<'a, 'b>(&'a self, v: &'b Persistent<A>) -> Concatenated<'a, 'b, A> {
         Concatenated {
             left: self,
             right: v,
         }
     }
+
+    fn prepend<'a, 'b>(&'a self, v: &'b A) -> Prepended<'a, 'b, A> {
+        Prepended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn append<'a, 'b>(&'a self, v: &'b A) -> Appended<'a, 'b, A> {
+        Appended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn set<'a, 'b>(&'a self, i: usize, v: &'b A) -> Changed<'a, 'b, A> {
+        Changed {
+            index: i,
+            value: v,
+            rest: self
+        }
+    }
+
     fn index(&self, i: usize) -> &A {
         &self.vector[i]
     }
@@ -36,13 +82,37 @@ impl<A> ListThingy<A> for VectorWrapper<A> {
     }
 }
 
-impl<'i, 'j, A> ListThingy<A> for Concatenated<'i, 'j, A> {
-    fn concat<'a, 'b>(&'a self, v: &'b ListThingy<A>) -> Concatenated<'a, 'b, A> {
+impl<'i, 'j, A> Persistent<A> for Concatenated<'i, 'j, A> {
+    fn concat<'a, 'b>(&'a self, v: &'b Persistent<A>) -> Concatenated<'a, 'b, A> {
         Concatenated {
             left: self,
             right: v,
         }
     }
+
+    fn prepend<'a, 'b>(&'a self, v: &'b A) -> Prepended<'a, 'b, A> {
+        Prepended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn append<'a, 'b>(&'a self, v: &'b A) -> Appended<'a, 'b, A> {
+        Appended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn set<'a, 'b>(&'a self, i: usize, v: &'b A) -> Changed<'a, 'b, A> {
+        Changed {
+            index: i,
+            value: v,
+            rest: self
+        }
+    }
+
+
     fn index(&self, i: usize) -> &A {
         if i < self.left.length() {
             self.left.index(i)
@@ -51,37 +121,152 @@ impl<'i, 'j, A> ListThingy<A> for Concatenated<'i, 'j, A> {
             self.right.index(i - self.left.length())
         }
     }
+
     fn length(&self) -> usize {
         self.left.length() + self.right.length()
     }
 }
 
-// impl<'a, 'b: 'a, A> ListThingy<'a, 'b, A> for VectorWrapper<A> {
-//     fn concat(&self, v: &'b ListThingy<'a, 'b, A>) -> Concatenated<A> {
-//         Concatenated {
-//             left: self,
-//             right: v,
-//         }
-//     }
-//     fn index(&self, i: usize) -> &A {
-//         self.vector[i]
-//     }
-// }
+
+impl<'i, 'j, A> Persistent<A> for Prepended<'i, 'j, A> {
+    fn concat<'a, 'b>(&'a self, v: &'b Persistent<A>) -> Concatenated<'a, 'b, A> {
+        Concatenated {
+            left: self,
+            right: v,
+        }
+    }
+
+    fn prepend<'a, 'b>(&'a self, v: &'b A) -> Prepended<'a, 'b, A> {
+        Prepended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn append<'a, 'b>(&'a self, v: &'b A) -> Appended<'a, 'b, A> {
+        Appended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn set<'a, 'b>(&'a self, i: usize, v: &'b A) -> Changed<'a, 'b, A> {
+        Changed {
+            index: i,
+            value: v,
+            rest: self
+        }
+    }
 
 
+    fn index(&self, i: usize) -> &A {
+        if i == 0 {
+            self.value
+        }
+        else {
+            self.rest.index(i - 1)
+        }
+    }
 
-pub fn greetings() -> String {
-    let a = VectorWrapper{
-        vector: vec![1, 2]
-    };
-    let b = VectorWrapper{
-        vector: vec![3, 4]
-    };
+    fn length(&self) -> usize {
+        self.rest.length() + 1
+    }
+}
 
-    println!("{}", a.index(1));
+impl<'i, 'j, A> Persistent<A> for Appended<'i, 'j, A> {
+    fn concat<'a, 'b>(&'a self, v: &'b Persistent<A>) -> Concatenated<'a, 'b, A> {
+        Concatenated {
+            left: self,
+            right: v,
+        }
+    }
 
-    let c = a.concat(&b);
-    println!("{}", c.index(1));
-    println!("{}", c.index(3));
-    return "Test".to_string();
+    fn prepend<'a, 'b>(&'a self, v: &'b A) -> Prepended<'a, 'b, A> {
+        Prepended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn append<'a, 'b>(&'a self, v: &'b A) -> Appended<'a, 'b, A> {
+        Appended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn set<'a, 'b>(&'a self, i: usize, v: &'b A) -> Changed<'a, 'b, A> {
+        Changed {
+            index: i,
+            value: v,
+            rest: self
+        }
+    }
+
+
+    fn index(&self, i: usize) -> &A {
+        if i == self.rest.length() {
+            self.value
+        }
+        else {
+            self.rest.index(i)
+        }
+    }
+
+    fn length(&self) -> usize {
+        self.rest.length() + 1
+    }
+}
+
+impl<'i, 'j, A> Persistent<A> for Changed<'i, 'j, A> {
+    fn concat<'a, 'b>(&'a self, v: &'b Persistent<A>) -> Concatenated<'a, 'b, A> {
+        Concatenated {
+            left: self,
+            right: v,
+        }
+    }
+
+    fn prepend<'a, 'b>(&'a self, v: &'b A) -> Prepended<'a, 'b, A> {
+        Prepended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn append<'a, 'b>(&'a self, v: &'b A) -> Appended<'a, 'b, A> {
+        Appended {
+            value: v,
+            rest: self
+        }
+    }
+
+    fn set<'a, 'b>(&'a self, i: usize, v: &'b A) -> Changed<'a, 'b, A> {
+        Changed {
+            index: i,
+            value: v,
+            rest: self
+        }
+    }
+
+
+    fn index(&self, i: usize) -> &A {
+        if i == self.index {
+            self.value
+        }
+        else {
+            self.rest.index(i)
+        }
+    }
+
+    fn length(&self) -> usize {
+        self.rest.length()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
 }
